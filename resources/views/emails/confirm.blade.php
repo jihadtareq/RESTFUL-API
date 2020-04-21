d7b87db77055aada216da8cf740949f252633f07
Jihad tarek <jihadtarek60@gmail.com>
2020-04-21 08:32:42 +0200

    MarkDown

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,5 +1,12 @@
-Hello {{$user->name}}
+@component('mail::message')
+# Hello {{$user->name}}
 
 You changed your email, so we need to verify this new address. Please use the button below: 
 
-{{route('verify', $user->verification_token)}}
+@component('mail::button', ['url' => route('verify', $user->verification_token)])
+Verify Account
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
