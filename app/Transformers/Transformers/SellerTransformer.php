ef2c2147e66d191f0c3e92d145fdf86ecda0d9ad
Jihad tarek <jihadtarek60@gmail.com>
2020-04-23 04:20:35 +0200

    FractalTransformer

---
+++ app/Transformers/Transformers/SellerTransformer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Transformers\Transformers;
+
+use App\Seller;
+use League\Fractal\TransformerAbstract;
+
+class SellerTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+    
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+    
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Transformers/Transformers/UserTransformer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Transformers\Transformers;
+
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+    
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+    
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            'identifier' => (int)$user->id,
+            'name' => (string)$user->name,
+            'email' => (string)$user->email,
+            'isVerified' => (int)$user->verified,
+            'isAdmin' => ($user->admin === 'true' ),
+            'creationDate' => $user->created_at,
+            'lastChange' => $user->updated_at,
+            'deletedDate' => isset($user->deleted_at) ? (string) $user->deleted_at : null,
+        
+        ];
+    }
+}
