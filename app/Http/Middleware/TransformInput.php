212794266f0fa25619b836fb73c0cd0f48c352d4
Jihad tarek <jihadtarek60@gmail.com>
2020-05-09 01:53:05 +0200

    transform validation

--- app/Http/Middleware/TransformInput.php
+++ app/Http/Middleware/TransformInput.php
@@ -13,8 +13,15 @@ class TransformInput
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
+    public function handle($request, Closure $next , $transformer)
+    {   $transformInput=[];
+
+        //using request  attribut then request to obtain all the fields
+        foreach ($request->request-all() as $input => $value) {
+              $transformInput[$transformer::originalAttribute($input)] =$value; 
+        }
+        //replace the original request by the new one
+        $request->replace($transformInput);
         return $next($request);
     }
 }
