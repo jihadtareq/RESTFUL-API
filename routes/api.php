8fd82a387b39f3d3ebb10c6046120b561e3e3789
Jihad tarek <jihadtarek60@gmail.com>
2020-05-13 08:05:23 +0200

    oauth/token

--- routes/api.php
+++ routes/api.php
@@ -65,5 +65,5 @@ User
 Route::resource('users','User\UserController',['except'=>['create','edit']]);
 Route::name('verify')->get('users/verify/{token}','User\UserController@verify');
 Route::name('resend')->get('users/{user}/resend','User\UserController@resend');
-
+Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
 
